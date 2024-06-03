937621c012e1e65d6fa3870e2c40d132534a16b5
Anish Shrestha <aneesh.sht@gmail.com>
2024-06-03 16:49:12 +1000

    changes in index

--- index.php
+++ index.php
@@ -7,7 +7,24 @@
     <?php
     include "includes/navigation.php";
     ?>
+    <div class="container">
+        <div class="d-flex">
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4"></div>
 
+        </div>
+        <div class="div">
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+            <div class="col-md-3"></div>
+        </div>
+    </div>
 </body>
 
 </html>
---
+++ product.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<?php include_once "includes/header.php";
+?>
+
+<body>
+	<div class="container">
+		<div>
+			<div><img src=""></div>
+			<div>
+				text desc button
+			</div>
+		</div>
+		<div>
+
+			Review rating
+
+			text box
+
+		</div>
+	</div>
+</body>
+
+</html>
